7c9e3d6606d26b73386fa7942c34bf02f7d6b796
Andreas Frey Sang <freysang@gmail.com>
2012-10-29 09:52:11 +0100

    Behance API Integration

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-Portfolio
-=========
+# Portfolio
+Portfolio page of xinteractionx
+
+# Dependencies
+[1140 CSS Grid](http://cssgrid.net/)
+[jQuery](http://jquery.com)
+[Responsive Measure](http://jbrewer.github.com/Responsive-Measure/)
+[galleria.io](http://galleria.io/)
+[tinycarousel](http://baijs.nl/tinycarousel/)
+[backbone.js](http://backbonejs.org)
+[underscorejs](http://underscorejs.org)
+
+# Behance Application
+Fetch and render projects from [Behance](http://www.behance.net/).
+KIM: if you want to add functionality to the app, you might want have a look
+at the [Backbone extension from Behance](https://github.com/behance/network_api_backbone).
+We decided against using it since we need to cache the json for projects anyway 
+(limited number of calls allowed by Behance) and load them from file. It is 
+therefore sufficient to use Backbone.
+
+## Update script
+PHP script that fetches all projects on behance and writes them to a file
+called _projects.json_.
+
+### During development
+You want to call this script everytime the projects change on Behance.
+
+If you serve the page through a web server (e.g. Apache), you can just point
+your browser to behanceUpdateScript.php.
+
+Otherwise, type on a shell (terminal on Mac) the command `php /path/to/behanceUpdateScript.php`.
+e.g. if your project is located in your home in a folder named _Portfolio_, you'd
+type `php ~/Portfolio/behanceUpdateScript.php
+
+### On live server
+Let a cronjob call this script on a regular basis. On Hostpoint, you'd install
+it as follows:
+1. upload your whole project (including behanceUpdateScript.php) to the server
+2. Log into the control panel
+3. Go to "Admin" > "Cronjobs Manager" and click on "Cronjob erstellen"
+4. To run it every 15 minutes, enter "*/15" for _m_ (minutes) and "*" for all 
+other time related fields; enter "php /pathToScript" for command, 
+e.g. "php ~/public_html/behanceUpdateScript.php" if the project is located directly in public_html
+5. Click "Erstellen"
+If everything works, the script will create and fill _projects.json_ when
+it is run the next time (which in above sample will be the next quarter of an hour).
+If anything goes wrong, the script will output an error message which will be 
+emailed to you.
+
+# TODOs
+
+## Behance Application
+The styling done so far is very limited to nonexistant.
+
+### Navigation
+* works for 1140px or wider, but needs adaption for smaller screens. See the 
+ 	media query at the end of behance.css for a styling sample to make the 
+ 	navigation half the size.
+* make sure previous/next buttons stay next to viewport and don't flow above/below
+* make sure pager is displayed centered
+
+### Stage
+Main issue here will be styling of the Galleria. I've added a theme to 
+js/galleria/themes/portfolio where I added some basic styling. Note that the
+controls have a red background so you can properly see them. 
+* handle configuration of gallery in js/galleria/themes/portfolio/galleria.portfolio.js
+* handle styling of gallery in js/galleria/themes/portfolio/galleria.portfolio.css
+* note that you can use js/galleria/themes/portfolio/portfolio-demo.html while styling
+* to replace the control icons, either change portfolio-map.png (looks like a 
+sprite, keep controls in their respective place) or change using css 
+
+
+## Add Parallax effect
+* [jparallax](http://stephband.info/jparallax/)
+* [curtain](http://editsquarterly.com/)
+* [stellar.js](http://markdalgleish.com/projects/stellar.js/)
+  * [Tutorial](http://webdesign.tutsplus.com/tutorials/complete-websites/create-a-parallax-scrolling-website-using-stellar-js) looks pretty similar to what we want
+  * could interfere with css3-mediaqueries.js (did when I tried to adapt the sample to 1140)
+  * [One Page Websites tagged with: Parallax Scrolling](http://onepagelove.com/tag/parallax-scrolling)
+  * Found only one example that is responsive as well: http://www.duluthtrading.com/site/components/last-pants-standing -> consider disabling effect with media queries
+
+### Showcases
+[Martin Oberhäuser](http://www.oberhaeuser.info)
+
+## Wordpress
+* 1140 used in other templates as well, e.g. [1140 Fluid Starkers WordPress Theme](http://www.thedotmack.com/2011/07/19/1140-fluid-starkers-wordpress-theme)
+* decide whether to work with pages or posts (from what makes sense in regard to information structure)
+  * assume pages:
+	- make onepager: probably just adapt template to render all pages on one (see http://stackoverflow.com/questions/7023310/wordpress-display-all-pages-on-one-page)
+	- adapt template to only render what we need
+  		- disable side-bar and comment box in page.php
+	- put fixedwrapper in header.php
+
+  * assume posts: make categories for home/work/who
+
+# Other
+[Markdown CheatSheet](http://warpedvisions.org/projects/markdown-cheat-sheet/)  
+[Markdown CheatSheet](https://en.wikipedia.org/wiki/Markdown)
---
+++ js/behance.js
@@ -0,0 +1,221 @@
+/** namespace for behance app */
+var BEHANCEAPP = {
+    views: {},
+    router: {},
+
+	/**
+	 *  Start App: Initiate the router and start history (needed for bookmarkable URL's)
+	 */
+    init: function() {
+	    new BEHANCEAPP.router();
+	    Backbone.history.start();
+    }
+}
+
+
+	
+/** ===========================================================================
+ * Responsible for displaying navigation. Consists of
+ * - list/carousel with thumbnail images
+ * - previous/next buttons
+ * - pager
+ * 
+ * uses tinycarousel.
+ * KIM that tinycarousel starts element count at 0 but page count at 1
+ * ===========================================================================*/
+BEHANCEAPP.views.Navigation = Backbone.View.extend({
+
+	el: $('nav'),
+	elementsVisible: 4,
+	currentPage: 1,
+	templates: {
+		listItem:_.template($('#tplNavListItem').html()),
+		pagerItem: _.template($('#tplNavPagerItem').html()),
+		
+	},
+	lookup: {},
+	
+	initialize: function () {
+		_.bindAll(this, 'handleCarouselChanges');
+	},
+	
+	/**
+	 * populate navigation from projects, populate lookup table
+	 */
+	render: function (projects) {
+		var self=this, i=0,
+			list = $('.overview'),
+			pager = $('.pager'),
+			nofPages = Math.floor(projects.length/this.elementsVisible);
+		
+		//remove any content present
+		list.empty();
+		pager.empty();
+		
+		//populate list/carousel
+		projects.each(function (project) {
+			var data = project.attributes,
+				resolutions  = _.keys(data.covers);
+			//select image with best resolution available
+			data.cover = data.covers[_.max(resolutions)];
+			self.lookup[data.id] = i++;
+			list.append(self.templates.listItem(data));
+		});
+		
+		//render pager items; KIM that (page-)ids have to start at 0 here
+		for (j=0; j<=nofPages; j++) {
+			pager.append(self.templates.pagerItem({index: j}));
+			
+		}
+		//start tinycarousel
+		this.$el.tinycarousel({
+			display: self.elementsVisible, // move elementsVisible blocks at a time
+			controls: true, // show left and right navigation button
+			pager: true, // display pager
+			callback: self.handleCarouselChanges
+		});
+	},
+	
+	/**
+	 *  mark entry with id as current
+	 */
+	setActive: function (id) {
+		var newPage = Math.floor(this.lookup[id] / this.elementsVisible) + 1;
+		this.$el.find('.overview li').removeClass('selected');
+		this.$el.find(".overview [data-id='" + id + "']").addClass('selected');
+		if (newPage != this.currentPage) {
+			this.$el.tinycarousel_move(newPage);
+			this.currentPage = newPage;
+		}
+	},
+	
+	handleCarouselChanges: function (element, index) {
+		/* KIM: element is always the element at position index, but index 
+		 * behaves differently for elementsVisible != 1: index corresponds then
+		 * to page-1 */
+		this.currentPage = index+1;
+	}
+});
+
+
+
+/** ===========================================================================
+ * Responsible for displaying content
+ * ===========================================================================*/
+BEHANCEAPP.views.Stage = Backbone.View.extend({
+	el: $('#stage'),
+	model: null,
+	template: _.template($('#tplStage').html()),
+	
+	/**
+	 * display element and start galleria
+	 */
+	render: function (item) {
+		item = this.prepare(item);
+		this.$el.html(this.template(item.attributes));
+		if (item.get('images').length > 0) {
+			Galleria.run('#galleria');
+		}
+	},
+	
+	/**
+	 * extract/prepare some data if necessary
+	 */
+	prepare: function(item) {
+		var data = item.attributes;
+		if ((data.byline === undefined) || (data.longDescription === undefined) || (data.images === undefined)) {
+			var longDescription = '', images = [], byline = '';
+			if (data.modules && data.modules.length > 0) {
+				/* extract byline from first and long description from second module entry by convention */
+				if (data.modules[0].type == 'text') {
+					byline = data.modules[0].text_plain;
+				}
+				if (data.modules[1].type == 'text') {
+					longDescription = data.modules[1].text_plain;
+				}
+				_.each(data.modules, function (module) {
+					if (module.type == 'image') {
+						images.push({
+							thumb: module.src, 
+							original: module.sizes.original,
+							caption: module.caption || '',
+						});
+					}
+				});
+			}
+			item.set('longDescription', longDescription);
+			item.set('images', images);
+			item.set('byline', byline);
+		};
+		return item;
+	}
+});
+
+
+
+/**===========================================================================
+ * Responsible for handling urls and loading projects
+ * ===========================================================================*/
+BEHANCEAPP.router = Backbone.Router.extend({
+    routes: {
+        "works/:id": "defaultRoute"
+    },
+	views : [],
+	selectedProjectId : null,
+	
+	initialize: function() {
+		var user, self=this, string, newString;
+		this.views.nav = new BEHANCEAPP.views.Navigation();
+		this.views.stage = new BEHANCEAPP.views.Stage();
+		this.projects = new Backbone.Collection();
+		
+		/* make sure views react to changed project list */
+		this.projects.on('reset', function() {
+			self.views.nav.render(self.projects);
+			self.updateViews();
+		});
+		this.initializeFromFile();
+	},
+	
+	/**
+	 * reset projects from string. used for development since it prevents
+	 * any issues related to asynchronity. remove for productive environment.
+	 */
+	initializeFromString: function() {
+		var string = '[{"id":5055813,"name":"Der Bote der Urschweiz","published_on":1346870931,"created_on":1346870207,"modified_on":1350575118,"url":"http:\/\/www.behance.net\/gallery\/Der-Bote-der-Urschweiz\/5055813","fields":["Interaction Design","User Interface Design","Web Design"],"covers":{"115":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/5055813\/115x089cc79e9a2d9b3cd4f2d8d1e8562fa5.png","202":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/5055813\/089cc79e9a2d9b3cd4f2d8d1e8562fa5.png","404":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/5055813\/404x089cc79e9a2d9b3cd4f2d8d1e8562fa5.png","230":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/5055813\/230x089cc79e9a2d9b3cd4f2d8d1e8562fa5.png"},"mature_content":0,"owners":{"1182455":{"id":1182455,"first_name":"Andreas","last_name":"Frey Sang","username":"freysang","city":"Bern","state":"","country":"Switzerland","company":"","occupation":"Aspiring Interaction Designer, Screendesigner, Typograf","created_on":1337613545,"url":"http:\/\/www.behance.net\/freysang","display_name":"Andreas Frey Sang","images":{"32":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/32x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","50":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/50x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","78":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/78x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","115":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/115x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","129":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/129x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","138":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/3dafdc37cac37adfe7c4ce866dd1f9cc.jpg"},"fields":["Graphic Design","Interaction Design","Print Design"]}},"stats":{"views":6,"appreciations":0,"comments":0}},{"id":3995717,"name":"Jugend Macht!","published_on":1337629826,"created_on":1337628804,"modified_on":1346879437,"url":"http:\/\/www.behance.net\/gallery\/Jugend-Macht\/3995717","fields":["Graphic Design","Print Design"],"covers":{"115":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/3995717\/115x634cd0fe8ed1540f1c69b204f2ff56b0.png","202":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/projects\/3995717\/634cd0fe8ed1540f1c69b204f2ff56b0.png"},"mature_content":0,"owners":{"1182455":{"id":1182455,"first_name":"Andreas","last_name":"Frey Sang","username":"freysang","city":"Bern","state":"","country":"Switzerland","company":"","occupation":"Aspiring Interaction Designer, Screendesigner, Typograf","created_on":1337613545,"url":"http:\/\/www.behance.net\/freysang","display_name":"Andreas Frey Sang","images":{"32":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/32x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","50":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/50x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","78":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/78x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","115":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/115x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","129":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/129x3dafdc37cac37adfe7c4ce866dd1f9cc.jpg","138":"http:\/\/behance.vo.llnwd.net\/profiles9\/1182455\/3dafdc37cac37adfe7c4ce866dd1f9cc.jpg"},"fields":["Graphic Design","Interaction Design","Print Design"]}},"stats":{"views":31,"appreciations":1,"comments":0}}]';
+		this.projects = new Behance.ProjectsCollection(JSON.parse(string));
+	},
+	
+	/**
+	 * reset projects from projects.json file
+	 */
+	initializeFromFile: function() {
+		var self=this;
+		$.get('projects.json', function(data) {
+			self.projects.reset(data);
+		});
+	},
+    
+    defaultRoute: function( id ) {
+    	this.selectedProjectId = id;
+    	//if projects are already loaded, update view
+    	if (this.projects.length > 0) {
+    		this.updateViews();
+    	}// else reset of this.projects will trigger updateViews
+    },
+    
+    /**
+     * provide respective data to views for rendering
+     */
+    updateViews: function () {
+    	var project;
+    	if (!this.selectedProjectId) {
+    		project = this.projects.models[0];
+    		this.selectedProjectId = project.id;
+    	} else {
+        	project = this.projects.get(this.selectedProjectId);
+    	}
+		this.views.stage.render(project);
+		this.views.nav.setActive(this.selectedProjectId);
+    }
+});
